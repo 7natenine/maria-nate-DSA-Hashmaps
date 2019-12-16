ee275fc2df48be807a2ee420ab2f3b2ba0345c7a
Maria Danielson <mal3905@gmail.com>
2019-12-16 12:34:22 -0500

    drill 1-2 done

--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -17,8 +17,40 @@ function main(){
     lotr.set('HalfElven','Arwen');
     lotr.set('Ent','Treebeard');
 
-    console.log(lotr);
-    console.log(lotr.get('Hobbit'));
+    // console.log(lotr);
+    // console.log(lotr.get('Hobbit'));
+    // console.log(lotr.get('Maiar'))
 }
 
 main();
+
+// #1 : 
+// Hobbit: Frodo and for Maiar: Sauron.
+// The discrepency is that it only shows the second value, and the first value, 
+// ignoring it. 
+// 
+// The capacity only 8, leaving number 9 as undefined. 
+
+
+// #2 
+const WhatDoesThisDo = function(){
+    let str1 = 'Hello World.';
+    let str2 = 'Hello World.';
+    let map1 = new hashMap();
+    map1.set(str1,10);
+    map1.set(str2,20);
+    let map2 = new hashMap();
+    let str3 = str1;
+    let str4 = str2;
+    map2.set(str3,20);
+    map2.set(str4,10);
+
+    console.log(map1.get(str1)); 
+    console.log(map2.get(str3)); 
+}
+WhatDoesThisDo();
+
+// String 1 is mapped to 10 but then is mapped to 20 and str 2 is 20, but then mapped to 10. 
+
+
+//#3 
