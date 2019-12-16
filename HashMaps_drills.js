5f671f144475adf10cb10d44a02d1caf86d47735
Nathan Leung <nath.leung@gmail.com>
2019-12-16 09:16:20 -0800

    question 1

--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -1,11 +1,24 @@
-HashMap require('./index')
+hashMap = require('./index')
 
 
-function main() {
+function main(){
+    let lotr = new hashMap();
+    lotr.MAX_LOAD_RATIO = 0.5;
+    lotr.SIZE_RATION = 3;
+    lotr.set('Hobbit','Bilbo');
+    lotr.set('Hobbit', 'Frodo');
+    lotr.set('Wizard','Gandolf');
+    lotr.set('Human','Aragon');
+    lotr.set('Elf','Legolas');
+    lotr.set('Maiar','The Necromancer');
+    lotr.set('Maiar', 'Sauron');
+    lotr.set('RingBearer','Gollum');
+    lotr.set('LadyOfLight','Galadriel');
+    lotr.set('HalfElven','Arwen');
+    lotr.set('Ent','Treebeard');
 
-    class lotrs {
-
-        
-    }
-
+    console.log(lotr);
+    console.log(lotr.get('Hobbit'));
 }
+
+main();
